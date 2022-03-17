611354aa5daa85ac599a6e4b06855e06ceda4574
Jonathan Mitchell <jonathanmitchell@Jonathans-MBP.BCMAIN.BRIGHTONCENTER.COM>
2022-03-17 15:37:34 -0400

    added IntersectionObserver JS scroll event

--- newscript.js
+++ newscript.js
@@ -1,6 +1,15 @@
-var img = new Image();
-img.src = 'image2.png';
-img.onclick = function() {
-    window.location.href = 'http://www.brightoncenter.com/';
-};
-document.body.appendChild(img);
+const images = document.querySelectorAll('.big-stat');
+
+observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add('big-stat');
+    } else {
+      entry.target.classList.remove('big-stat');
+    }
+  });
+});
+
+images.forEach(image => {
+  observer.observe(image);
+});
