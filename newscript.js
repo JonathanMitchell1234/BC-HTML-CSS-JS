d32877cf7babe9d31fe3179d994525b845c50d5b
Jonathan Mitchell <jonathanmitchell@Jonathans-MacBook-Pro.local>
2022-03-18 14:21:24 -0400

    added donation link intersection observer in JS

--- newscript.js
+++ newscript.js
@@ -14,4 +14,22 @@ images.forEach(image => {
   observer.observe(image);
 });
 
+const link = document.querySelectorAll('.donate-now-link');
+
+observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add('donate-now-link');
+    } else {
+      entry.target.classList.remove('donate-now-link');
+    }
+  });
+});
+
+link.forEach(link => {
+  observer.observe(link);
+});
+
+
+
 
