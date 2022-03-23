60cc7bdbddf1e326afa027849321de1de9c68128
JonathanMitchell1234 <88846650+JonathanMitchell1234@users.noreply.github.com>
2022-03-23 11:39:53 -0400

    started script for countup animation property statistics

--- newscript.js
+++ newscript.js
@@ -35,3 +35,109 @@ link.forEach(link => {
 
 
 
+function animateValue(id, start, end, duration) {
+  // assumes integer values for start and end
+  
+  var obj = document.getElementById(id);
+  var range = end - start;
+  // no timer shorter than 50ms (not really visible any way)
+  var minTimer = 50;
+  // calc step time to show all interediate values
+  var stepTime = Math.abs(Math.floor(duration / range));
+  
+  // never go below minTimer
+  stepTime = Math.max(stepTime, minTimer);
+  
+  // get current time and calculate desired end time
+  var startTime = new Date().getTime();
+  var endTime = startTime + duration;
+  var timer;
+
+  function run() {
+      var now = new Date().getTime();
+      var remaining = Math.max((endTime - now) / duration, 0);
+      var value = Math.round(end - (remaining * range));
+      obj.innerHTML = value;
+      if (value == end) {
+          clearInterval(timer);
+      }
+  }
+  
+  timer = setInterval(run, stepTime);
+  run();
+}
+
+animateValue("value", 0, 207, 5000);
+
+
+
+
+function animateValue(id, start, end, duration) {
+  // assumes integer values for start and end
+  
+  var obj = document.getElementById(id);
+  var range = end - start;
+  // no timer shorter than 50ms (not really visible any way)
+  var minTimer = 50;
+  // calc step time to show all interediate values
+  var stepTime = Math.abs(Math.floor(duration / range));
+  
+  // never go below minTimer
+  stepTime = Math.max(stepTime, minTimer);
+  
+  // get current time and calculate desired end time
+  var startTime = new Date().getTime();
+  var endTime = startTime + duration;
+  var timer;
+
+  function run() {
+      var now = new Date().getTime();
+      var remaining = Math.max((endTime - now) / duration, 0);
+      var value = Math.round(end - (remaining * range));
+      obj.innerHTML = value;
+      if (value == end) {
+          clearInterval(timer);
+      }
+  }
+  
+  timer = setInterval(run, stepTime);
+  run();
+}
+
+animateValue("value2", 0, 12, 5000);
+
+
+function animateValue(id, start, end, duration) {
+  // assumes integer values for start and end
+  
+  var obj = document.getElementById(id);
+  var range = end - start;
+  // no timer shorter than 50ms (not really visible any way)
+  var minTimer = 50;
+  // calc step time to show all interediate values
+  var stepTime = Math.abs(Math.floor(duration / range));
+  
+  // never go below minTimer
+  stepTime = Math.max(stepTime, minTimer);
+  
+  // get current time and calculate desired end time
+  var startTime = new Date().getTime();
+  var endTime = startTime + duration;
+  var timer;
+
+  function run() {
+      var now = new Date().getTime();
+      var remaining = Math.max((endTime - now) / duration, 0);
+      var value = Math.round(end - (remaining * range));
+      obj.innerHTML = value;
+      if (value == end) {
+          clearInterval(timer);
+      }
+  }
+  
+  timer = setInterval(run, stepTime);
+  run();
+}
+
+animateValue("value3", 0, 31, 5000);
+
